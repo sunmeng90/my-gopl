a170c27248de0991708ac638af8bcbb40a1d7393
Meng <sunmeng90@126.com>
2019-08-01 22:51:47 +0800

    ch3 pass pointer to string in function

--- ex3.11/commaFloatSign.go
+++ ex3.11/commaFloatSign.go
@@ -1,47 +1,49 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+)
 import "strings"
 
 func comma(s string) string {
 	var buf bytes.Buffer
-	intSign(&buf, s)
+	intSign(&buf, &s)
 	firstNumIdx := 0
 	if strings.HasPrefix(s, "-") || strings.HasPrefix(s, "+") {
 		firstNumIdx = 1
 	}
 	dotIdx := strings.LastIndex(s, ".")
 	if dotIdx == -1 {
-		intComma(&buf, s, firstNumIdx, len(s)-1)
+		intComma(&buf, &s, firstNumIdx, len(s)-1)
 	} else {
-		intComma(&buf, s, firstNumIdx, dotIdx-1)
-		intAfterDot(&buf, s, dotIdx+1)
+		intComma(&buf, &s, firstNumIdx, dotIdx-1)
+		intAfterDot(&buf, &s, dotIdx+1)
 	}
 	return buf.String()
 }
 
-func intSign(buf *bytes.Buffer, s string) {
-	if strings.HasPrefix(s, "-") {
+func intSign(buf *bytes.Buffer, s *string) {
+	if strings.HasPrefix(*s, "-") {
 		buf.WriteByte('-')
 	}
 }
 
 // add comma to integers
-func intComma(buf *bytes.Buffer, s string, start int, end int) {
+func intComma(buf *bytes.Buffer, s *string, start int, end int) {
 	re := (end - start + 1) % 3
 	intPart1EndIdx := start + re - 1
 	for i := start; i <= end; i++ {
-		buf.WriteByte(s[i])
+		buf.WriteByte((*s)[i])
 		if (i != end) && (i == intPart1EndIdx || (i-intPart1EndIdx)%3 == 0) {
 			buf.WriteString(",")
 		}
 	}
 }
 
-func intAfterDot(buf *bytes.Buffer, s string, idxAfterDot int) {
-	n := len(s)
+func intAfterDot(buf *bytes.Buffer, s *string, idxAfterDot int) {
+	n := len(*s)
 	buf.WriteByte('.')
 	for i := idxAfterDot; i < n; i++ {
-		buf.WriteByte(s[i])
+		buf.WriteByte((*s)[i])
 	}
 }
