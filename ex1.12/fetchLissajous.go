20ee8b6631a6b53d93638e6cd15a8cfba0590665
Meng <sunmeng90@126.com>
2019-08-11 11:01:11 +0800

    ch1 web serve parse Form

---
+++ ex1.12/fetchLissajous.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+	http.HandleFunc("/", lissajousHandler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func lissajousHandler(writer http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	cyclesStr := request.Form.Get("cycles")
+	var cycles = 5
+	if cyclesStr != "" {
+		var err error
+		cycles, err = strconv.Atoi(cyclesStr)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Get cycles: %v", err)
+		}
+	}
+	lissajous(writer, cycles)
+}
+
+func lissajous(out io.Writer, cycles int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
